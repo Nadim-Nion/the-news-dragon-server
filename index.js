a54f67e9e282c1f257d21ebb3b6a677b5649f1e0
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2023-12-24 12:46:55 +0600

    CORS added

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const categories = require('./data/categories.json');
 const news = require('./data/news.json');
 
 // app.use(cors());
-app.use(cors(
-    {
-        origin: '*',
-        credentials: true
-    }))
+
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     res.send('The Dragon News is running');
